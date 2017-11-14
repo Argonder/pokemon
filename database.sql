10520922acfd69d8a16b9647c81ca9029c7c67db
JodeQ <jodeq@jeucodeinformatique.fr>
2017-11-14 14:19:17 +0100

    Composer et authentification

---
+++ database.sql
@@ -0,0 +1,21 @@
+
+create table pokedex (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    nom_proprietaire VARCHAR(50) NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
+
+create table pokemon (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    numero INTEGER NOT NULL,
+    nom VARCHAR(50) NOT NULL,
+    experience INTEGER NOT NULL DEFAULT 0,
+    vie INTEGER NOT NULL,
+    defense INTEGER NOT NULL,
+    attaque INTEGER NOT NULL,
+    id_pokedex INTEGER,
+    PRIMARY KEY (id),
+    FOREIGN KEY (id_pokedex) REFERENCES pokedex(id)ON DELETE SET NULL
+);
+
+ALTER TABLE `pokemon` ADD (img_url VARCHAR(255));
--- public/index.php
+++ public/index.php
@@ -6,12 +6,25 @@ require_once('header.php');
 require_once('../resources/function.php');
 
 $errors = [];
+?>
 
-// Affichage du lien d'insertion
-echo '<a href="insert.php" class="btn btn-primary">Ajouter un pokemon</a>';
+<div class="container">
+  <form class="form-signin">
+    <h2 class="form-signin-heading">Se connecter</h2>
+    <label for="inputEmail" class="sr-only">Adresse Email</label>
+    <input type="email" id="inputEmail" class="form-control" placeholder="Adresse email" required autofocus>
+    <label for="inputPassword" class="sr-only">Mot de passe</label>
+    <input type="password" id="inputPassword" class="form-control" placeholder="Mot de passe" required>
+    <div class="checkbox">
+      <label>
+        <input type="checkbox" value="remember-me">Se souvenir
+      </label>
+    </div>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Connexion</button>
+  </form>
 
-// Affichage de la liste des pokemon
-affichePokemon();
+</div> <!-- /container -->
 
+<?php
 // Fin du HTML
 require_once('footer.php');
